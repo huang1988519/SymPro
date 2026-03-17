ac5f8f8a6ef400d90205ee52dbcdc28550682f76
hwh <wally.h@qq.com>
2026-03-17 11:39:17 +0800

    Initial Commit

---
+++ SymPro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SymPro
+//
+//  Created by 黄伟华 on 2026/3/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SymPro/SymProApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymProApp.swift
+//  SymPro
+//
+//  Created by 黄伟华 on 2026/3/17.
+//
+
+import SwiftUI
+
+@main
+struct SymProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
